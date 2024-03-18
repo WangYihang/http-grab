8367b9017ddc5d649527ec840679e3e8fd43d46b
Yihang Wang <wangyihanger@gmail.com>
2024-03-18 16:05:39 +0800

    feat: move option to pkg

--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"os"
-
 	"github.com/WangYihang/gojob"
 	"github.com/WangYihang/gojob/pkg/utils"
 	"github.com/WangYihang/http-grab/pkg/model"
-	"github.com/jessevdk/go-flags"
+	"github.com/WangYihang/http-grab/pkg/option"
 )
-
-type Options struct {
-	InputFilePath  string `short:"i" long:"input" description:"input file path" required:"true"`
-	OutputFilePath string `short:"o" long:"output" description:"output file path" required:"true"`
-	StatusFilePath string `short:"s" long:"status" description:"status file path" required:"true" default:"-"`
-
-	NumWorkers               int   `short:"n" long:"num-workers" description:"number of workers" default:"32"`
-	NumShards                int64 `long:"num-shards" description:"number of shards" default:"1"`
-	Shard                    int64 `long:"shard" description:"shard" default:"0"`
-	MaxTries                 int   `short:"m" long:"max-tries" description:"max tries" default:"4"`
-	MaxRuntimePerTaskSeconds int   `short:"t" long:"max-runtime-per-task-seconds" description:"max runtime per task seconds" default:"60"`
-
-	Port int    `short:"p" long:"port" description:"port" default:"80"`
-	Path string `long:"path" description:"path" default:"index.html"`
-	Host string `long:"host" description:"http host header, leave it blank to use the IP address" default:""`
-}
-
-var opts Options
-
-func init() {
-	_, err := flags.Parse(&opts)
-	if err != nil {
-		os.Exit(1)
-	}
-}
 
 func main() {
 	scheduler := gojob.NewScheduler().
-		SetNumWorkers(opts.NumWorkers).
-		SetMaxRetries(opts.MaxTries).
-		SetMaxRuntimePerTaskSeconds(opts.MaxRuntimePerTaskSeconds).
-		SetNumShards(int64(opts.NumShards)).
-		SetShard(int64(opts.Shard)).
-		SetOutputFilePath(opts.OutputFilePath).
+		SetNumWorkers(option.Opt.NumWorkers).
+		SetMaxRetries(option.Opt.MaxTries).
+		SetMaxRuntimePerTaskSeconds(option.Opt.MaxRuntimePerTaskSeconds).
+		SetNumShards(int64(option.Opt.NumShards)).
+		SetShard(int64(option.Opt.Shard)).
+		SetOutputFilePath(option.Opt.OutputFilePath).
 		Start()
-	for line := range utils.Cat(opts.InputFilePath) {
-		scheduler.Submit(model.NewTask(line, opts.Port, opts.Path, opts.Host))
+	for line := range utils.Cat(option.Opt.InputFilePath) {
+		scheduler.Submit(model.NewTask(line, option.Opt.Port, option.Opt.Path, option.Opt.Host))
 	}
 	scheduler.Wait()
 }
---
+++ pkg/option/option.go
@@ -0,0 +1,32 @@
+package option
+
+import (
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+type Option struct {
+	InputFilePath  string `long:"input" description:"input file path" required:"true"`
+	OutputFilePath string `long:"output" description:"output file path" required:"true"`
+	StatusFilePath string `long:"status" description:"status file path" required:"true" default:"-"`
+
+	NumWorkers               int   `long:"num-workers" description:"number of workers" default:"32"`
+	NumShards                int64 `long:"num-shards" description:"number of shards" default:"1"`
+	Shard                    int64 `long:"shard" description:"shard" default:"0"`
+	MaxTries                 int   `long:"max-tries" description:"max tries" default:"4"`
+	MaxRuntimePerTaskSeconds int   `long:"max-runtime-per-task-seconds" description:"max runtime per task seconds" default:"60"`
+
+	Port int    `long:"port" description:"port" default:"80"`
+	Path string `long:"path" description:"path" default:"index.html"`
+	Host string `long:"host" description:"http host header, leave it blank to use the IP address" default:""`
+}
+
+var Opt Option
+
+func init() {
+	_, err := flags.Parse(&Opt)
+	if err != nil {
+		os.Exit(1)
+	}
+}
